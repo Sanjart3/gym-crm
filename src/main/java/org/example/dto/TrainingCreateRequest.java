6c0f68907b1f6b57d25230cfa80696e5934e3620
Sanjar Totliboyev <sanjartotliboyev@gmail.com>
2024-07-26 17:46:13 +0500

    Training Creation Request is created

---
+++ src/main/java/org/example/dto/TrainingCreateRequest.java
@@ -0,0 +1,69 @@
+package org.example.dto;
+
+import java.time.LocalDate;
+
+public class TrainingCreateRequest {
+    private Long traineeId;
+    private Long trainerId;
+    private String trainingName;
+    private Long trainingType;
+    private LocalDate trainingDate;
+    private int trainingDuration;
+
+    public TrainingCreateRequest(Long traineeId, Long trainerId, String trainingName, Long trainingType, LocalDate trainingDate, int trainingDuration) {
+        this.traineeId = traineeId;
+        this.trainerId = trainerId;
+        this.trainingName = trainingName;
+        this.trainingType = trainingType;
+        this.trainingDate = trainingDate;
+        this.trainingDuration = trainingDuration;
+    }
+
+    public Long getTraineeId() {
+        return traineeId;
+    }
+
+    public void setTraineeId(Long traineeId) {
+        this.traineeId = traineeId;
+    }
+
+    public Long getTrainerId() {
+        return trainerId;
+    }
+
+    public void setTrainerId(Long trainerId) {
+        this.trainerId = trainerId;
+    }
+
+    public String getTrainingName() {
+        return trainingName;
+    }
+
+    public void setTrainingName(String trainingName) {
+        this.trainingName = trainingName;
+    }
+
+    public Long getTrainingType() {
+        return trainingType;
+    }
+
+    public void setTrainingType(Long trainingType) {
+        this.trainingType = trainingType;
+    }
+
+    public LocalDate getTrainingDate() {
+        return trainingDate;
+    }
+
+    public void setTrainingDate(LocalDate trainingDate) {
+        this.trainingDate = trainingDate;
+    }
+
+    public int getTrainingDuration() {
+        return trainingDuration;
+    }
+
+    public void setTrainingDuration(int trainingDuration) {
+        this.trainingDuration = trainingDuration;
+    }
+}
